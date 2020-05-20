f100f109b8d66cf51fd65a8ef90408eb3bdee41a
bmaha2 <maharjanbipin@gmail.com>
2020-05-19 17:42:04 -0700

    cleanup

--- employee.js
+++ employee.js
@@ -12,38 +12,7 @@ const employees = {
     })
   },
 
-//   create: function(name, age, sex, fixed){
-//     const sql = `INSERT INTO cats (pet_name, pet_age, pet_sex, desexed) VALUES (?, ?, ?, ?)`;
 
-//     return new Promise(function(resolve, reject){
-//       connection.query(sql, [name, age, sex, fixed], function(err, data) {
-//         if (err) reject(err);
-//         resolve(data);
-//       });
-//     })
-//   },
-
-//   update: function(desexed, id){
-//     const sql = `UPDATE cats SET desexed = ? WHERE id = ?`;
-
-//     return new Promise(function(resolve, reject){
-//       connection.query(sql, [desexed, id], function(err, data) {
-//         if (err) reject(err);
-//         resolve(data);
-//       });
-//     })
-//   },
-
-//   destroy: function(id){
-//     const sql = `DELETE FROM cats WHERE id = ?`;
-
-//     return new Promise(function(resolve, reject){
-//       connection.query(sql, [id], function(err, data) {
-//         if (err) reject(err);
-//         resolve(data);
-//       });
-//     })
-//   }
 }
 
 module.exports = employees;
